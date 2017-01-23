a7c7e4619d26678c9e3878456ad3b32e0f3d6b25
John Thompson <johnny918819@gmail.com>
2017-01-23 09:08:50 -0800

    test commit for practice

---
+++ src/whatYouKnowAboutProgramming/wwwroot/app.js
@@ -0,0 +1,1 @@
+console.log("The mos difficult thing about programming thus far has been the sheer amount of information");
---
+++ src/whatYouKnowAboutProgramming/wwwroot/app.ts
@@ -0,0 +1,1 @@
+﻿console.log("The mos difficult thing about programming thus far has been the sheer amount of information");
